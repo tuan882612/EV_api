d14e35697f5f3eea05d94636890905d5f37c941d
tuan <tuan882612@gmail.com>
2023-05-25 12:10:26 -0500

    bugfix: Enable Reactive Mongo

--- src/main/java/api/v1/App.java
+++ src/main/java/api/v1/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class App {
 
     public static void main(String[] args) {
